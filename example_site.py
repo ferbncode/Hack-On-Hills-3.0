ac9176b1f15695fc299fb4dd259ec44f60ee9041
Suyash Garg <suyashgargsfam@gmail.com>
2018-03-24 21:31:04 +0530

    Add example_site.py

---
+++ example_site.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template_string
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route("/")
+def hello():
+    return render_template_string("""
+    <html>
+    <title>hello1</title>
+    <body>Hello world 1 site
+        <script>
+            console.log("calling the backend");
+            let json_object;
+            function loadFunc() {
+                let xhttp = new XMLHttpRequest();
+                xhttp.open('GET', 'http://localhost:5001/pool', true);
+                xhttp.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        console.log(this.response);
+                        json_object = JSON.parse(this.response);
+                        const name = json_object.name;
+                        const url = json_object.url;
+
+                        console.log('Running job for process: ', name);
+                        var head = document.getElementsByTagName('head').item(0);
+                        let script = document.createElement('script');
+                        script.setAttribute('type','text/javascript');
+                        script.setAttribute('src', url);
+                        head.appendChild(script);
+                    }
+                };
+                xhttp.send();
+            }
+            loadFunc();
+        </script>
+    </body>
+    </html>""")
+
+app.run(debug=True, port=5000)
