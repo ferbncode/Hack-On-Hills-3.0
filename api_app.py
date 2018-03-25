92e15bd3d58a90b3043d5366c054f07f3dd026c0
Utkarsh Raj <clearnote01@gmail.com>
2018-03-25 16:19:17 +0530

    utkarsh commit

--- api_app.py
+++ api_app.py
@@ -4,13 +4,21 @@ from flask import request
 from flask import render_template_string
 from split import Job, divideData, Pool
 from flask_cors import CORS, cross_origin
+from flask_socketio import SocketIO, emit
 from queue import Queue
 import random
+import json
 import string
 
 app = Flask(__name__)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SECRET_KEY'] = 'this is the secreet key'
+sio = SocketIO(app)
+
+# active_connections = 
+    # {'socketId': 'running/available'}
+# 
+active_connections = {}
 
 @app.route("/")
 @cross_origin()
@@ -23,86 +31,10 @@ def hello():
         </body>
     """)
 
-code_is_alive = """
-        var running = setInterval(function() {
-            var xhttp = new XMLHttpRequest();
-            xhttp.open("GET", "http://localhost:5001/api/running/"+id, true);
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    console.log(this.response);
-                    json_object = JSON.parse(this.response);
-                    console.log(json_object);
-                    if (json_object.status == 0) {
-                        clearInterval(running);
-                    }
-                }
-            }
-            xhttp.send();
-        }, 4000);
-"""
-
 @app.route("/api")
 @cross_origin()
 def api():
-    return "This is the motherfucking api!"
-
-@app.route("/api/running/<id>")
-@cross_origin()
-def is_running(id):
-    print('Job '+id+'is running!');
-    data = {
-        'status': 1
-    }
-    json_data = jsonify(data)
-    print(request);
-    return json_data
-
-@app.route("/api/running/<id>/false")
-def false_id(id):
-    data = {
-        'status': 0
-    }
-
-
-
-@app.route("/api/sample1")
-@cross_origin()
-def sampleCode():
-    r = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(11)])
-
-    data_text = 'bimal kant'
-    unique_id = 'xdfzd'
-
-    with open('static/'+r+'.js', 'w') as f:
-        f.write(f"""
-            console.log('this is a sample.js file');
-            console.log('this is line 2 of file');
-            data = "{data_text}";
-            data_fixed = data.split(' ').join('');
-            console.log(data_fixed);
-            id = "{unique_id}";
-            var xhttp = new XMLHttpRequest();
-            xhttp.open("POST", "http://localhost:5001/api/postresult/"+id, true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.send("result="+data_fixed);
-        \n"""+code_is_alive)
-
-    data = {
-        'id': 1,
-        'url': 'http://localhost:5001/static/'+r+'.js',
-        'name': 'Join user names'
-    }
-    json_data = jsonify(data)
-    return json_data
-
-@app.route('/api/postresult/<id>',methods=['POST'])
-@cross_origin()
-def result(id):
-    data_result = request.form['result']
-    print('result computed for : ', id)
-    print(data_result)
-    return 'hELO'
-
+    return "This is the api!"
 
 job_queue = Queue()
 current_job = None
@@ -120,53 +52,6 @@ def add_job():
 
 # We do a more discrete division for the array elements
 # for systems with Lesser Ram
-@app.route('/pool', methods=['GET'])
-@cross_origin()
-def get_from_pool():
-    global current_job
-    global job_queue
-    if not current_job:
-        current_job = job_queue.get()
-    try:
-        partitioned_job = current_job.division.pool.getTopJob()
-    except Exception as e:
-        return jsonify({
-            "message": "No more jobs in the pool",
-        })
-
-    r = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(11)])
-
-    data_text = partitioned_job['data']
-    unique_id = partitioned_job['id']
-    code = partitioned_job['code']
-    # data_text = 'suyash garg'
-    # unique_id = r
-    # code = """data_fixed = data.split(' ').join('');"""
-
-    with open('static/'+r+'.js', 'w') as f:
-        f.write(f"""
-            console.log('this is a sample.js file');
-            console.log('this is line 2 of file');
-            data = "{data_text}";
-            {code}
-            console.log(data_fixed);
-            id = "{unique_id}";
-            var xhttp = new XMLHttpRequest();
-            xhttp.open("POST", "http://localhost:5001/api/postresult/"+id, true);
-            xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-            xhttp.send("result="+data_fixed);
-        \n"""+code_is_alive)
-
-    data = {
-        'id': unique_id,
-        'url': 'http://localhost:5001/static/'+r+'.js',
-        'name': 'Join user names'
-    }
-    json_data = jsonify(data)
-    return json_data
-    # return jsonify(partitioned_job)
-
-
 @app.route('/pool', methods=['POST'])
 @cross_origin()
 def post_to_pool():
@@ -174,12 +59,78 @@ def post_to_pool():
     current_job.merge_results(data)
     print(f"Completed stuff here! - Here is the result - {data}")
 
+@sio.on('givemejob')
+def job_from_pool(message):
+    print('getting from pool via sockets')
+    # global current_job
+    # global job_queue
+    # if not current_job:
+        # current_job = job_queue.get()
+    # try:
+        # partitioned_job = current_job.division.pool.getTopJob()
+    # except Exception as e:
+        # return jsonify({
+            # "message": "No more jobs in the pool",
+        # })
+
+    r = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(11)])
+
+    # data_text = partitioned_job['data']
+    # unique_id = partitioned_job['id']
+    # code = partitioned_job['code']
+    data_text = 'suyash garg'
+    unique_id = r
+    code = """data_fixed = data.split(' ').join('');"""
+
+    with open('static/'+r+'.js', 'w') as f:
+        f.write(f"""
+            data = "{data_text}";
+            {code}
+            id = "{unique_id}";
+	    postMessage({{id,data_fixed}});
+        \n""")
+
+    data = {
+        'id': unique_id,
+        'url': 'http://localhost:5001/static/'+r+'.js',
+        'name': 'Join user names'
+    }
+    json_data = json.dumps(data)
+    print('json data: ')
+    print(json_data)
+    emit('pool job', json_data)
+    return ''
 
 @app.route('/job', methods=['GET'])
 @cross_origin()
-def gandu_function():
+def end_function():
     return jsonify({
         "message": "jab job khatam ho jaayegi to post kar denge?",
     })
 
-app.run(host='127.0.0.1',port=5001,debug=True)
+@sio.on('postingresult')
+def result(message):
+  print('getting result from socket')
+  print(message)
+  return ''
+
+@sio.on('currentstatus')
+def status_update(message):
+  print(message)
+  active_connections[request.sid] = "busy"
+  return ''
+
+@sio.on('connect')
+def handle_con():
+    active_connections[request.sid] = 'available'
+    print(request.sid)
+
+@sio.on('disconnect')
+def handle_dis():
+    active_connections.pop(request.sid)
+    print('available connections: ')
+    print(active_connections)
+    print(request.sid, ' disconnected ...')
+
+if __name__ == '__main__':
+    sio.run(app,port=5001,debug=False)
--- example_site.py
+++ example_site.py
@@ -9,30 +9,8 @@ def hello():
     <html>
     <title>hello1</title>
     <body>Hello world 1 site
-        <script>
-            console.log("calling the backend");
-            let json_object;
-            function loadFunc() {
-                let xhttp = new XMLHttpRequest();
-                xhttp.open('GET', 'http://localhost:5001/pool', true);
-                xhttp.onreadystatechange = function() {
-                    if (this.readyState == 4 && this.status == 200) {
-                        console.log(this.response);
-                        json_object = JSON.parse(this.response);
-                        const name = json_object.name;
-                        const url = json_object.url;
-
-                        console.log('Running job for process: ', name);
-                        var head = document.getElementsByTagName('head').item(0);
-                        let script = document.createElement('script');
-                        script.setAttribute('type','text/javascript');
-                        script.setAttribute('src', url);
-                        head.appendChild(script);
-                    }
-                };
-                xhttp.send();
-            }
-            loadFunc();
+        <script type="text/javascript" src="static/client.js"></script>
+	<script>
         </script>
     </body>
     </html>""")
